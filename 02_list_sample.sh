7e7db87d713bc97a56096ac82159d5f69ee301ba
takahiro-maekawa <koma4024@gmail.com>
2025-03-10 00:04:49 +0900

    文字アレイ検索用のスクリプトを作成

--- 02_list_sample.sh
+++ 02_list_sample.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 INPUT=$1
 
-cat ./query/05_select_string_sample.sql | 
-  sed "s/__VAR__/${INPUT}/" |
+tokens=$(curl -s -X 'POST' \
+    "http://localhost:13000/tokenize" \
+    -H "accept: text/plain" \
+    -H "Content-Type: application/json" \
+    -d "{
+    \"text\": \"${INPUT}\"
+}"  | jq -c | sed "s/\"/'/g")
+
+# 改行をエスケープ
+
+echo "$tokens"
+
+cat ./query/06_select_list_template.sql | 
+  sed "s/__VAR__/${tokens}/" |
   curl 'http://localhost:18123/?password=changeme' --data-binary @-
